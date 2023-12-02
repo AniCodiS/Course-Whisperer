39664534ef0f10300ce7a4f1c04169aa1eecd05d
Mariam Kuloshvili <mkulos@cryptx.com>
2023-12-02 20:21:02 +0400

    initial commit

---
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/CourseWhispererApplication.java
@@ -0,0 +1,13 @@
+package com.freeuni.coursewhisperer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWhispererApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWhispererApplication.class, args);
+    }
+
+}
---
+++ CourseWhisperer/src/test/java/com/freeuni/coursewhisperer/CourseWhispererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeuni.coursewhisperer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWhispererApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
