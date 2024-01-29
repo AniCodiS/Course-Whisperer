06a0d616661f2d2ba7d27a16d3c6824dbbe0df8e
Ani Odishelidze <ani.odishelidze@azry.com>
2024-01-29 12:56:20 +0400

    implement getting username of logged in user

--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/config/GetLoggedInUser.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/config/GetLoggedInUser.java
@@ -3,6 +3,8 @@ package com.freeuni.coursewhisperer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public class GetLoggedInUser {
 
@@ -11,8 +13,19 @@ public class GetLoggedInUser {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            return "Current User: " + username;
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof OidcUser) {
+                // For OpenID Connect (OAuth2) authentication
+                OidcUser oidcUser = (OidcUser) principal;
+                return "Current User: " + oidcUser.getPreferredUsername();
+            } else if (principal instanceof OAuth2User) {
+                // For generic OAuth2 authentication
+                OAuth2User oauth2User = (OAuth2User) principal;
+                return "Current User: " + oauth2User.getName();
+            } else {
+                return "Unsupported principal type: " + principal.getClass().getName();
+            }
         } else {
             return "User not authenticated";
         }
