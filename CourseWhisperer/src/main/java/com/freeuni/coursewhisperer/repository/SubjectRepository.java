677cefcb46035256a7a88c14f020322bdfcdfa93
Mariam Kuloshvili <mkulos@cryptx.com>
2024-02-04 15:58:02 +0400

    added mapper and repository classes

---
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/SubjectRepository.java
@@ -0,0 +1,6 @@
+package com.freeuni.coursewhisperer.repository;
+
+import com.freeuni.coursewhisperer.data.entity.SubjectEntity;
+
+public interface SubjectRepository extends AbstractRepository<SubjectEntity, Long> {
+}
