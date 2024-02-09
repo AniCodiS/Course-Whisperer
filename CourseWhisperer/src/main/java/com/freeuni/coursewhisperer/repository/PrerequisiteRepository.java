f1e6844b4c163d1bd80b0fa81b4d42e6faec5620
Mariam Kuloshvili <mkulos@cryptx.com>
2024-02-09 16:06:56 +0400

    add and refactor repositories

--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/PrerequisiteRepository.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/PrerequisiteRepository.java
@@ -1,14 +1,14 @@
 package com.freeuni.coursewhisperer.repository;
 
-import com.freeuni.coursewhisperer.data.entity.Prerequisite;
+import com.freeuni.coursewhisperer.data.entity.PrerequisiteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrerequisiteRepository extends JpaRepository<Prerequisite, Long> {
+public interface PrerequisiteRepository extends JpaRepository<PrerequisiteEntity, Long> {
     boolean existsPrerequisiteById(Long id);
 
-    Prerequisite findPrerequisiteById(Long id);
+    PrerequisiteEntity findPrerequisiteById(Long id);
 
     void deletePrerequisiteById(Long id);
 }
--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/SubjectRepository.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/SubjectRepository.java
@@ -1,10 +1,10 @@
 package com.freeuni.coursewhisperer.repository;
 
-import com.freeuni.coursewhisperer.data.entity.Subject;
+import com.freeuni.coursewhisperer.data.entity.SubjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-    Subject findBySubjectName(String name);
+public interface SubjectRepository extends JpaRepository<SubjectEntity, Long> {
+    SubjectEntity findByName(String name);
 }
