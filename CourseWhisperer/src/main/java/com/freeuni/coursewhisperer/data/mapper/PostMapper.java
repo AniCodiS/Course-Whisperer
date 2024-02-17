9c2aa6deb4e8e0f95e656f63d11608769a52482a
Mariam Kuloshvili <mkulos@cryptx.com>
2024-02-17 15:23:52 +0400

    methods added in post repository

--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/data/mapper/PostMapper.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/data/mapper/PostMapper.java
@@ -1,10 +1,10 @@
 package com.freeuni.coursewhisperer.data.mapper;
 
-import com.freeuni.coursewhisperer.data.api.PostDto;
+import com.freeuni.coursewhisperer.data.api.dto.PostDTO;
 import com.freeuni.coursewhisperer.data.entity.PostEntity;
 import com.freeuni.coursewhisperer.data.model.Post;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper extends AbstractMapper<PostEntity, Post, PostDto> {
+public interface PostMapper extends AbstractMapper<PostEntity, Post, PostDTO> {
 }
--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/PostRepository.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/repository/PostRepository.java
@@ -1,6 +1,14 @@
 package com.freeuni.coursewhisperer.repository;
 
 import com.freeuni.coursewhisperer.data.entity.PostEntity;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends AbstractRepository<PostEntity, Long>{
+    @Query("select p from PostEntity p order by p.upVote desc ")
+    List<PostEntity> getPosts();
+
+    Optional<PostEntity> findById(Long id);
 }
