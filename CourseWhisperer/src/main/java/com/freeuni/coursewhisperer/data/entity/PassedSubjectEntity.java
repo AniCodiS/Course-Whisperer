0e3faeba953c947221435153674640fc2ebc6885
Mariam Kuloshvili <mkulos@cryptx.com>
2024-02-17 16:37:32 +0400

    passed subject entity changed

--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/data/entity/PassedSubjectEntity.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/data/entity/PassedSubjectEntity.java
@@ -2,26 +2,29 @@ package com.freeuni.coursewhisperer.data.entity;
 
 import com.freeuni.coursewhisperer.data.entity.common.AbstractIdTimestampEntity;
 import com.freeuni.coursewhisperer.data.enums.EGrade;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "passed_subject")
 public class PassedSubjectEntity extends AbstractIdTimestampEntity {
-    @OneToOne // TODO change all onetoone
-    @JoinColumn(name = "student")
-    private UserEntity student;
+    @Column(name = "student")
+    private Long student;
 
-    @OneToOne
-    @JoinColumn(name = "subject")
-    private SubjectEntity subject;
+    @Column(name = "subject")
+    private Long subject;
 
     @Column(name = "grade")
     private EGrade grade;
 
     @Column(name = "grade_score")
-    private int gradeScore;
+    private Integer gradeScore;
 }
