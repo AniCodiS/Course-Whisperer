04886693d02318704e9f6b5af0afdc1289db86be
Mariam Kuloshvili <mkulos@cryptx.com>
2024-02-17 16:38:00 +0400

    security added

--- CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/config/SecurityConfig.java
+++ CourseWhisperer/src/main/java/com/freeuni/coursewhisperer/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,14 +15,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(HttpMethod.POST, "/api/").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/api/").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/api/").authenticated()
-                        .anyRequest().permitAll())
-                .httpBasic();
+                        .anyRequest().permitAll());
         return http.build();
     }
 }
