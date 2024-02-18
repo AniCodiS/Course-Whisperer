656f30edbd79b95210c285abda7417039ff20afb
Mariam Kuloshvili <mkulos@cryptx.com>
2024-02-18 13:38:35 +0400

    imports optimized

--- backend/src/main/java/com/freeuni/coursewhisperer/data/validator/CorrectText.java
+++ backend/src/main/java/com/freeuni/coursewhisperer/data/validator/CorrectText.java
@@ -1,14 +1,8 @@
 package com.freeuni.coursewhisperer.data.validator;
 
 import jakarta.validation.Constraint;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.*;
-import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+
 import java.lang.annotation.*;
 
 @Documented
