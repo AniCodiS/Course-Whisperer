c84e0ccb88bb2ff1c8acb8296127d160c58ad0d5
AniCodiS <anii.odishelidze@gmail.com>
2024-04-08 22:12:51 +0400

    integrate leaderboard

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,16 +1,22 @@
-// Leaderboard.js
-
-import React, { useState } from 'react';
-import '../styles/Leaderboard.css'; // Import CSS file for Leaderboard page
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../styles/Leaderboard.css';
 
 const Leaderboard = () => {
-    // Hardcoded data for leaderboard
-    const [users, setUsers] = useState([
-        { id: 1, name: 'John Doe', score: 95 },
-        { id: 2, name: 'Alice Smith', score: 88 },
-        { id: 3, name: 'Bob Johnson', score: 82 },
-        // Add more users as needed
-    ]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchLeaderboard = async () => {
+            try {
+                const response = await axios.get('http://localhost:8081/api/user-score/all');
+                setUsers(response.data.sort((a, b) => b.score - a.score)); // Sort users based on score in descending order
+            } catch (error) {
+                console.error('Error fetching leaderboard:', error);
+            }
+        };
+
+        fetchLeaderboard();
+    }, []);
 
     return (
         <div className="leaderboard-container">
@@ -19,19 +25,19 @@ const Leaderboard = () => {
                 <div className="column">
                     <p className="header">Place</p>
                     {users.map((user, index) => (
-                        <p key={user.id}>{index + 1}</p>
+                        <p key={user.username}>{index + 1}</p>
                     ))}
                 </div>
                 <div className="column">
-                    <p className="header">Name</p>
+                    <p className="header">Username</p>
                     {users.map((user) => (
-                        <p key={user.id}>{user.name}</p>
+                        <p key={user.username}>{user.username}</p>
                     ))}
                 </div>
                 <div className="column">
                     <p className="header">Score</p>
                     {users.map((user) => (
-                        <p key={user.id}>{user.score}</p>
+                        <p key={user.username}>{user.score}</p>
                     ))}
                 </div>
             </div>
