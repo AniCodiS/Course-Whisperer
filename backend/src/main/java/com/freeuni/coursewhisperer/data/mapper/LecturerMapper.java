af8058231e74c65622473b203296115febb3b448
Mariam Kuloshvili <mkulos@cryptx.com>
2024-04-10 13:56:35 +0400

    search lecturers added

--- backend/src/main/java/com/freeuni/coursewhisperer/data/mapper/LecturerMapper.java
+++ backend/src/main/java/com/freeuni/coursewhisperer/data/mapper/LecturerMapper.java
@@ -3,7 +3,6 @@ package com.freeuni.coursewhisperer.data.mapper;
 import com.freeuni.coursewhisperer.data.api.dto.LecturerDTO;
 import com.freeuni.coursewhisperer.data.entity.LecturerEntity;
 import com.freeuni.coursewhisperer.data.model.Lecturer;
-import com.freeuni.coursewhisperer.service.LecturerService;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
--- backend/src/main/java/com/freeuni/coursewhisperer/repository/LecturerRepository.java
+++ backend/src/main/java/com/freeuni/coursewhisperer/repository/LecturerRepository.java
@@ -1,7 +1,10 @@
 package com.freeuni.coursewhisperer.repository;
 
 import com.freeuni.coursewhisperer.data.entity.LecturerEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LecturerRepository extends AbstractRepository<LecturerEntity, Long> {
@@ -10,4 +13,8 @@ public interface LecturerRepository extends AbstractRepository<LecturerEntity, L
     LecturerEntity findByEmail(String email);
 
     void deleteByEmail(String email);
+
+    @Query("select l from LecturerEntity l where " +
+            "(:emails is null or l.email in :emails)")
+    List<LecturerEntity> searchByEmails(List<String> emails);
 }
