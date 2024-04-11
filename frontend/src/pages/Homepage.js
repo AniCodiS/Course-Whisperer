0ae611e23779688f5fb2a24f35d3da326645c53b
Mariam Kuloshvili <mkulos@cryptx.com>
2024-04-11 23:04:30 +0400

    put inside row scope

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -15,54 +15,58 @@ const Homepage = () => {
         <div className="homepage-container">
             <h1 className="title">Welcome to Course Whisperer</h1>
             <div className="page-list">
-                <Link to="/personal-information" className="page-link">
-                    <div className="page-item">
-                        <img src={personalInformationImage} alt="Personal Information" className="page-img" />
-                        <span className="page-text">Personal Information</span>
-                    </div>
-                </Link>
-                <Link to="/change-password" className="page-link">
-                    <div className="page-item">
-                        <img src={ChangePasswordImage} alt="Change Password" className="page-img" />
-                        <span className="page-text">Change Password</span>
-                    </div>
-                </Link>
-                <Link to="/grades" className="page-link">
-                    <div className="page-item">
-                        <img src={gradesImage} alt="Grades" className="page-img" />
-                        <span className="page-text">Grades</span>
-                    </div>
-                </Link>
-                <Link to="/choose-subjects" className="page-link">
-                    <div className="page-item">
-                        <img src={chooseSubjectsImage} alt="Choose Subjects" className="page-img" />
-                        <span className="page-text">Choose Subjects</span>
-                    </div>
-                </Link>
-                <Link to="/gpa-calculator" className="page-link">
-                    <div className="page-item">
-                        <img src={gpaCalculatorImage} alt="GPA Calculator" className="page-img" />
-                        <span className="page-text">GPA Calculator</span>
-                    </div>
-                </Link>
-                <Link to="/study-groups" className="page-link">
-                    <div className="page-item">
-                        <img src={studyGroupsImage} alt="Study Groups" className="page-img" />
-                        <span className="page-text">Study Groups</span>
-                    </div>
-                </Link>
-                <Link to="/student-forum" className="page-link">
-                    <div className="page-item">
-                        <img src={studentForumImage} alt="Student Forum" className="page-img" />
-                        <span className="page-text">Student Forum</span>
-                    </div>
-                </Link>
-                <Link to="/leaderboard" className="page-link">
-                    <div className="page-item">
-                        <img src={leaderboardImage} alt="Leaderboard" className="page-img" />
-                        <span className="page-text">Leaderboard</span>
-                    </div>
-                </Link>
+                <div className="row">
+                    <Link to="/personal-information" className="page-link">
+                        <div className="page-item">
+                            <img src={personalInformationImage} alt="Personal Information" className="page-img" />
+                            <span className="page-text">Personal Information</span>
+                        </div>
+                    </Link>
+                    <Link to="/change-password" className="page-link">
+                        <div className="page-item">
+                            <img src={ChangePasswordImage} alt="Change Password" className="page-img" />
+                            <span className="page-text">Change Password</span>
+                        </div>
+                    </Link>
+                    <Link to="/grades" className="page-link">
+                        <div className="page-item">
+                            <img src={gradesImage} alt="Grades" className="page-img" />
+                            <span className="page-text">Grades</span>
+                        </div>
+                    </Link>
+                    <Link to="/choose-subjects" className="page-link">
+                        <div className="page-item">
+                            <img src={chooseSubjectsImage} alt="Choose Subjects" className="page-img" />
+                            <span className="page-text">Choose Subjects</span>
+                        </div>
+                    </Link>
+                </div>
+                <div className="row">
+                    <Link to="/gpa-calculator" className="page-link">
+                        <div className="page-item">
+                            <img src={gpaCalculatorImage} alt="GPA Calculator" className="page-img" />
+                            <span className="page-text">GPA Calculator</span>
+                        </div>
+                    </Link>
+                    <Link to="/study-groups" className="page-link">
+                        <div className="page-item">
+                            <img src={studyGroupsImage} alt="Study Groups" className="page-img" />
+                            <span className="page-text">Study Groups</span>
+                        </div>
+                    </Link>
+                    <Link to="/student-forum" className="page-link">
+                        <div className="page-item">
+                            <img src={studentForumImage} alt="Student Forum" className="page-img" />
+                            <span className="page-text">Student Forum</span>
+                        </div>
+                    </Link>
+                    <Link to="/leaderboard" className="page-link">
+                        <div className="page-item">
+                            <img src={leaderboardImage} alt="Leaderboard" className="page-img" />
+                            <span className="page-text">Leaderboard</span>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
