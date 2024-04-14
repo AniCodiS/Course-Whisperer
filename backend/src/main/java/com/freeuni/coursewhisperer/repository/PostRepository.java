d91d7ae332fe1f47be86eba20b1b79597c2e4efd
Mariam Kuloshvili <mkulos@cryptx.com>
2024-04-14 10:32:35 +0400

    post repository refactor

--- backend/src/main/java/com/freeuni/coursewhisperer/repository/PostRepository.java
+++ backend/src/main/java/com/freeuni/coursewhisperer/repository/PostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository extends AbstractRepository<PostEntity, Long>{
+public interface PostRepository extends AbstractRepository<PostEntity, Long> {
     @Query("select p from PostEntity p order by p.upVote desc ")
     List<PostEntity> getPosts();
 
@@ -15,9 +15,8 @@ public interface PostRepository extends AbstractRepository<PostEntity, Long>{
 
     @Query("select p from PostEntity p where " +
             "(:username is null or p.username =:username) and " +
-            "(:subject is null or p.subject = :subject) and " +
+            "(:subject is null or p.subject like concat('%',  :subject, '%')) and " +
             "(:type is null or p.type = :type) order by p.upVote desc ")
-
     List<PostEntity> searchPosts(String username, String subject, EPostType type);
 
     void deleteById(Long id);
