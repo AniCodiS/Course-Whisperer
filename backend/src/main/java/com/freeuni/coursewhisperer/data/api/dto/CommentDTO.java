486559ddd33444750f4d0f56281173f93e4360ad
Mariam Kuloshvili <mkulos@cryptx.com>
2024-04-16 15:14:18 +0400

    comment service added

--- backend/src/main/java/com/freeuni/coursewhisperer/data/api/dto/CommentDTO.java
+++ backend/src/main/java/com/freeuni/coursewhisperer/data/api/dto/CommentDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentDTO {
+    private Long id;
     private String username;
     private Long postId;
     private String content;
